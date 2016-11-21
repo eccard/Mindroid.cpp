5298b4a1a4c9b57a5d55720460578db2e8d0b84b
Felipe Lugão Eccard <feccard@muxi.com.br>
2016-11-21 18:51:36 -0200

    fix ndk-build -C jni/Mindroid.cpp

--- Android.mk
+++ Android.mk
@@ -73,11 +73,13 @@ LOCAL_SRC_FILES := \
 	src/mindroid/util/logging/LogBuffer.cpp \
 	src/mindroid/util/logging/Logger.cpp
 
-LOCAL_CFLAGS := -DANDROID -Wno-unused-parameter -fexceptions
+LOCAL_CFLAGS := -DANDROID -Wno-unused-parameter -fexceptions -c -g -O0 -fPIC -fexceptions -std=c++11 -landroid
+LOCAL_CPPFLAGS := -frtti -landroid
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE:= libmindroid
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 LOCAL_RTTI_FLAG := -frtti
 include $(BUILD_STATIC_LIBRARY)
+
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ Application.mk
@@ -0,0 +1,6 @@
+#APP_STL := gnustl_static
+APP_STL := c++_static
+#NDK_TOOLCHAIN_VERSION :=4.9
+APP_ABI := armeabi-v7a
+#APP_ABI := armeabi armeabi-v7a
+APP_PLATFORM := android-21
